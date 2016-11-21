ade1a0bdf590ed624967737886f7419798df4de9
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-11-21 11:38:22 +0100

    add toUpper and toLower methods

--- src/stringClass.js
+++ src/stringClass.js
@@ -6,6 +6,25 @@ const StringManipulation = {
   hasVowels() {
     return /[aeiou]/i.test(this);
   },
+  /**
+   * Converts a string to uppercase characters
+   * @return{String} - string in uppercase
+   */
+  toUpper() {
+    return this.replace(/[a-z]/g, character =>
+      String.fromCharCode(character.charCodeAt(0) - 32)
+    );
+  },
+
+  /**
+   * Converts a string to uppercase characters
+   * @return{String} - string in uppercase
+   */
+  toLower() {
+    return this.replace(/[A-Z]/g, character =>
+      String.fromCharCode(character.charCodeAt(0) + 32)
+    );
+  }
 };
 
 Object.assign(String.prototype, StringManipulation);
